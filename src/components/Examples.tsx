0ab62851ca04d999cd562b298b254e6b7dbde1ab
Oksana <ksu1ven@mail.ru>
2024-04-17 15:51:57 +0300

    feat: add swiper

---
+++ src/components/Examples.tsx
@@ -0,0 +1,57 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Keyboard, A11y, Scrollbar } from 'swiper/modules';
+import cat from '@assets/images/cat.jpg';
+
+import 'swiper/scss';
+import 'swiper/scss/navigation';
+import 'swiper/scss/a11y';
+import 'swiper/scss/scrollbar';
+
+export function Examples() {
+    const slidesPerView = window.innerWidth > 768 ? 2 : 1;
+
+    const EXAMPLES = new Array(8).fill(cat);
+
+    return (
+        <section className="examples-bg">
+            <div className="examples wrapper">
+                <h2 className="examples__h2">Примеры наших работ</h2>
+                <div className="examples__content">
+                    <Swiper
+                        className="examples-swiper"
+                        modules={[Navigation, Keyboard, A11y, Scrollbar]}
+                        spaceBetween={50}
+                        slidesPerView={slidesPerView}
+                        speed={600}
+                        navigation
+                        keyboard
+                        scrollbar={{
+                            hide: true,
+                        }}
+                        a11y={{
+                            prevSlideMessage: 'Previous slide',
+                            nextSlideMessage: 'Next slide',
+                        }}
+                    >
+                        {EXAMPLES.map((el) => (
+                            <SwiperSlide
+                                className="examples-slide"
+                                key={el.name}
+                            >
+                                <img
+                                    src={el}
+                                    alt="work example"
+                                    className="examples__img"
+                                />
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+                    <p className="examples__p">
+                        Выполняем работы любой сложности и дизайна с учётом
+                        Ваших пожеланий.
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
+}
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,6 +1,7 @@
 import { Greeting } from '@components/Greeting';
 import { WorkTypes } from '@components/WorkTypes';
 import { Header } from '@components/Header';
+import { Examples } from '@components/Examples';
 
 export function LandingPage() {
     return (
@@ -9,6 +10,7 @@ export function LandingPage() {
             <main>
                 <Greeting />
                 <WorkTypes />
+                <Examples />
             </main>
         </>
     );
