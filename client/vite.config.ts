b705e5d4f2d5b69e82c7e69df3f6436f10850b09
Oksana <ksu1ven@mail.ru>
2024-04-20 22:47:58 +0300

    feat: add server

---
+++ client/vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@assets': path.resolve(__dirname, 'src/assets'),
+            '@utils': path.resolve(__dirname, 'src/utils'),
+            '@pages': path.resolve(__dirname, 'src/pages'),
+            '@components': path.resolve(__dirname, 'src/components'),
+        },
+    },
+    server: {
+        port: 3000,
+    },
+});
---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const bodyparcer = require("body-parser");
+const nodemailer = require("nodemailer");
+const cors = require("cors");
+
+const app = express();
+app.use(bodyparcer.json());
+app.use(bodyparcer.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/", () => {
+  console.log("WELCOME");
+  resizeBy.send("Welcome!");
+});
+
+app.post("/api/send", (req, res) => {
+  console.log("ASDFG");
+  console.log(req.body);
+  let { name, phone, description } = req.body;
+
+  let smtpTransport = nodemailer.createTransport({
+    host: "smtp.mail.ru",
+    port: 465,
+    secure: true,
+    auth: {
+      user: "ksu1ven@mail.ru",
+      pass: "rmK3TK9AE5XCLqx4JJWs",
+    },
+  });
+
+  let mailoptions = {
+    from: "ksu1ven@mail.ru",
+    to: "ksu1ven@mail.ru",
+    subject: "Новый заказ с сайта",
+    html: `
+        <h3>${name}</h3>
+        <h3>${phone}</h3>
+        <h3>${description}</h3>
+        `,
+  };
+
+  smtpTransport.sendMail(mailoptions, (error, response) => {
+    if (error) res.send(error);
+    else {
+      res.send("Success");
+    }
+  });
+
+  smtpTransport.close();
+});
+
+const PORT = process.env.PORT || 3001;
+
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "/dist")));
+
+  // Handle React routing, return all requests to React app
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "/dist", "index.html"));
+  });
+}
+
+app.listen(PORT, () => {
+  console.log(`Server on port ${PORT}`);
+});
