a5b6a6fde3f7069943bdd40ddcba70104d9b1330
Oksana <ksu1ven@mail.ru>
2024-05-11 14:36:47 +0300

    fix: fix

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ const router = createBrowserRouter(
     createRoutesFromElements(
         <>
             <Route path="/" element={<LandingPage />} />
-            <Route path="*" element={<main>error page</main>} />
+            <Route path="*" element={<main>error</main>} />
         </>
     )
 );
